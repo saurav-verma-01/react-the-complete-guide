b65b336853c63670d99550151632755424e95023
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-11-05 02:45:30 +0530

    learning react

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,5 +1,18 @@
+import Player from "./components/Player";
+
 function App() {
-  return <p>Coming Soon</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 01" symbol="X" />
+          <Player name="Player 02" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+      LOG RESULTS
+    </main>
+  );
 }
 
 export default App;
